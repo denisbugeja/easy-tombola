8671dff6191a22a4d1392b0f557c76451e72a543
Denis BUGEJA <denis.bugeja@gmail.com>
2021-11-28 17:30:27 +0100

    1ère version focntionelle

--- generator.js
+++ generator.js
@@ -5,19 +5,69 @@ var ticketsPerPage = 5,
 document.addEventListener("DOMContentLoaded", function () {
 
     document.querySelector('button#print').addEventListener('click', function () {
-        let vals = getListOfValues();
-        if(!vals){
-            alert('Problème lors de la génération des tickets, veuillez revoir les valeurs saisies');
-            return;
-        }
+        document.querySelector('#ticketlist').innerHTML = generateAllTickets();
         window.print();
     });
 
+    // preview
+    document.querySelectorAll("#generator input").forEach(function (input) {
+        ['input'].forEach(function (event) {
+            input.addEventListener(event, function (e) {
+                document.querySelector('#preview').innerHTML = generateOneTicket(1);
+            });
+        });
+    });
 
-    for (let i = 0; i < ticketsPerPage; i++) {
-        ticketList.innerHTML += sample;
-    }
+    document.querySelector('#preview').innerHTML = generateOneTicket(1);
+
 });
+
+/**
+ * @returns string
+ */
+function generateAllTickets() {
+    let allPositions = getListOfValues();
+    if (!allPositions) {
+        alert('Problème lors de la génération des tickets, veuillez revoir les valeurs saisies');
+        return;
+    }
+    let allTicketHtml = '';
+    allPositions.forEach(function (position) {
+        allTicketHtml += generateOneTicket(position);
+    });
+    return allTicketHtml;
+}
+
+/**
+ * 
+ * @param {number} position 
+ * @returns string
+ */
+function generateOneTicket(position) {
+    let parser = new DOMParser(),
+        ticket = parser.parseFromString(sample, "text/html"),
+        formattedPosition = position.toString(),
+        ticketsPerStack = parseInt(document.querySelector('#tickets_per_stack').value),
+        stackNumber = parseInt(document.querySelector('#stack_number').value),
+        maxTicketNumber = stackNumber * ticketsPerStack,
+        prefixNumber = maxTicketNumber.toString().length - formattedPosition.length;
+
+    for (let i = 0; i < prefixNumber; i++) {
+        formattedPosition = '0' + formattedPosition;
+    }
+
+    ticket.querySelectorAll('.number').forEach(function (number) {
+        number.innerHTML = formattedPosition;
+    });
+
+    document.querySelectorAll("#generator [data-mapto]").forEach(function (input) {
+        ticket.querySelectorAll('.' + input.getAttribute('data-mapto')).forEach(function (element) {
+            element.innerHTML = input.value;
+        });
+    });
+
+    return ticket.querySelector('.ticket').outerHTML;
+}
 
 /**
  * @returns Array|false
