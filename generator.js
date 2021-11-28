911cff4e62f7f848e3b172f50464383e2a617a0a
Denis BUGEJA <denis.bugeja@gmail.com>
2021-11-28 12:21:42 +0100

    WIP

--- generator.js
+++ generator.js
@@ -3,28 +3,47 @@ var ticketsPerPage = 5,
     ticketList = document.querySelector('#ticketlist');
 
 document.addEventListener("DOMContentLoaded", function () {
+
+    document.querySelector('button#print').addEventListener('click', function () {
+        let vals = getListOfValues();
+        if(!vals){
+            alert('Problème lors de la génération des tickets, veuillez revoir les valeurs saisies');
+            return;
+        }
+        window.print();
+    });
+
+
     for (let i = 0; i < ticketsPerPage; i++) {
         ticketList.innerHTML += sample;
     }
+});
 
-    /**
-     * 
-     * @param {string} val 
-     * @param {string} selector 
-     * @param {string} target 
-     */
-    function replaceValueForSelector(val, selector, target) {
-        let elements = document.querySelectorAll(target + ' ' + selector);
-        if (elements.length != 0) {
-            elements.innerHTML = val;
+/**
+ * @returns Array|false
+ */
+function getListOfValues() {
+    let arrGeneratedTickets = [],
+        ticketsPerStack = parseInt(document.querySelector('#tickets_per_stack').value),
+        stackNumber = parseInt(document.querySelector('#stack_number').value),
+        nbTickets = stackNumber * ticketsPerStack,
+        nbPages = nbTickets / ticketsPerPage,
+        coeff = (ticketsPerPage - 1) * ticketsPerStack;
+
+    for (let i = 0; i < nbPages; i++) {
+        for (let j = 0; j < ticketsPerPage; j++) {
+            let position = (i + 1) + (j * ticketsPerStack) + (coeff * parseInt(i / ticketsPerStack));
+            if (!(arrGeneratedTickets.includes(parseInt(position)))) {
+                arrGeneratedTickets.push(parseInt(position));
+            } else {
+                return false;
+                console.log('Attention, Doublons générés !! ' + position.toString());
+            }
         }
     }
-
-    /**
-     * @returns Array
-     */
-    function getListOfValues() {
-        return [];
+    if (arrGeneratedTickets.length < nbTickets) {
+        return false;
+        console.log("Attention tous les tickets n'ont pas été générés");
     }
-
-});
+    return arrGeneratedTickets;
+}
